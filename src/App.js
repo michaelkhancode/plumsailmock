3fc254035c3c245d4b9ad613f1c07f2465d02514
Michael Yohannes <yohannesbergnov@gmail.com>
2020-01-03 18:21:38 -0500

    initial project

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import RadioLive from './LiveFormElements/RadioLive';
+
+
+// Form Elements:
+//   *Required Tag question
+//   *Question 1, option for subtitle in italics for more context
+
+//   Radio Button     ->  Question, minimum 2 radios
+//                        Add more radios, swich to check all apply
+
+//   Text/Num Input   ->  Question minimum 1 box, switch to small/large box
+//                        Text or Numeric switch
+//                        Numiric, with restrictions >=, <= etc..
+
+//   Rating           ->  5 star default with option for 2 too 10, start symbol
+
+//   Date             ->  Enter a date with a date picker
+
+const useStyles = makeStyles({
+  wrapSurvey: {
+    margin:"auto",
+    minWidth: 275,
+    maxWidth: 700,
+  }
+});
+
+export default function SimpleCard() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapSurvey} >
+      < RadioLive />
+      <br/>
+      
+    </div>
+  );
+}
---
+++ src/LiveFormElements/RadioLive.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+const useStyles = makeStyles({  
+  maintitle: {
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 14,
+    fontStyle: "italic",
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  flexWrapper: {
+      display:"flex",
+  },
+  numbering: {
+    marginRight: 3,
+  }
+});
+
+//   Radio Button     ->  Question, minimum 2 radios
+//                        Add more radios, swich to check all apply
+
+export default function RadioLive() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState('female');
+
+  const handleChange = event => {
+    console.log(event.target)
+    setValue(event.target.value);
+  };
+
+  return (
+    <div>
+        <Card className={classes.card}>
+            <CardContent>
+
+                <div className={classes.flexWrapper} >
+                    <div className={classes.numbering}>
+                        <Typography align="left" className={classes.maintitle} color="textPrimary">
+                            {`${1}.`}
+                        </Typography>
+                    </div>
+                    <div>
+                        <Typography align="left" className={classes.maintitle} color="textPrimary">
+                            What is your Gender
+                        </Typography>
+                        <Typography align="left" className={classes.subtitle} color="textPrimary">
+                            As per birth certificate
+                        </Typography>
+
+                        <FormControl className={classes.formControl}>
+                            <RadioGroup 
+                                aria-label="gender" 
+                                name="gender1" 
+                                value={value} 
+                                onChange={handleChange}
+                            >
+                                <FormControlLabel 
+                                    // submission info
+                                    value="female" 
+                                    // Radio button
+                                    control={<Radio size="small" />}
+                                    // What shows
+                                    label="Female"
+                                    style={{}}
+                                />
+                                <FormControlLabel
+                                    value="male" 
+                                    control={<Radio size="small" />} 
+                                    label="Male" 
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    </div>
+  );
+}
